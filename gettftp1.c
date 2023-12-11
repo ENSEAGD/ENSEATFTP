0a1487993daca898d2910c11d56ba952c790c11e
EliottDelannee <152154324+EliottDelannee@users.noreply.github.com>
2023-12-11 09:43:09 +0100

    Update gettftp1.c

--- gettftp1.c
+++ gettftp1.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
 
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        printf("Usage: %s <hostname> <filename>\n", argv[0]);
+        return 1;
+    }
+
+    char *hostname = argv[1];
+    char *filename = argv[2];
+
+    return 0;
+}
