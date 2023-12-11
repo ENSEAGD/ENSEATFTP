52b7c2e01f178a7fd46f0741cb467d5de9a9ac39
EliottDelannee <152154324+EliottDelannee@users.noreply.github.com>
2023-12-11 11:07:03 +0100

    Update gettftp2.c

--- gettftp2.c
+++ gettftp2.c
@@ -1,6 +1,10 @@
+#include <sys/types.h>
 #include <stdio.h>
-#include <netdb.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include <string.h>
+#include <sys/socket.h>
+#include <netdb.h>
 
 int main(int argc, char *argv[]) {
 
